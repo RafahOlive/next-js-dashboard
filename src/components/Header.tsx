40539b2a6c57a0ae554138a91cd59b740c43cf1f
Anderson Rodrigues <rafaelribeiro12@live.com>
2022-04-13 11:48:06 -0300

    Styling dashboard and components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,20 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 
 export default function Header(){
     return(
-        <Flex></Flex>
+        <Flex as="header" width="100%" maxWidth={1480} height="20"
+        marginX="auto" marginTop="4" paddingX="6" align="center"
+        >
+            <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight"
+            width="64"
+            >
+                Dashboard
+                <Text as="span" marginLeft="1" color="pink.500">.</Text>
+            </Text>
+
+            <Flex as="label" flex="1" paddingY="4" paddingX="8">
+
+            </Flex>
+        </Flex>
     );
 }
